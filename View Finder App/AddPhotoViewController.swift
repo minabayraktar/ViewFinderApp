863a6b7af52c99dce79be6198b001b3f176b4152
Apple <apple@apples-macbook-pro-50.local>
2019-06-24 13:04:58 -0700

    Complete Iteration 1

--- View Finder App/AddPhotoViewController.swift
+++ View Finder App/AddPhotoViewController.swift
@@ -8,15 +8,38 @@
 
 import UIKit
 
-class AddPhotoViewController: UIViewController {
-
+class AddPhotoViewController: UIViewController,
+UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var imagePicker = UIImagePickerController ()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func albumTapped(_ sender: Any) {
+        imagePicker.sourceType = .savedPhotosAlbum
+        present(imagePicker, animated: true, completion: nil)
+    }
+  
+   
+    @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker,animated : true, completion: nil)
+    }
+   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+    if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as?UIImage{
+        imageView.image = selectedImage
+        //go back to our ViewController so the user can see the update
+       
+    }
+     imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 

0d1faac0b97d8762fbaa933ac47595d6bc152fe9
Apple <apple@apples-macbook-pro-50.local>
2019-06-25 09:00:47 -0700

    Complete Iteration 3

---
+++ View Finder App/PhotoDetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  PhotoDetailViewController.swift
+//  View Finder App
+//
+//  Created by Apple on 6/25/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+    @IBOutlet weak var secondPhoto: UIImageView!
+    var photo : Photos? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let realPhoto = photo{
+            title = realPhoto.caption
+            
+            if let cellPhotoImageData = realPhoto.imageData {
+                if let cellPhotoImage = UIImage(data: cellPhotoImageData){
+       secondPhoto.image = cellPhotoImage
+                }
+            }
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- View Finder App/PhotoTableViewController.swift
+++ View Finder App/PhotoTableViewController.swift
@@ -50,8 +50,22 @@ class PhotoTableViewController: UITableViewController {
         // Configure the cell...
         return cell
     }
- 
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue (withIdentifier: "moveToDetail", sender: photos[indexPath.row])
+    }
+    
+        override func prepare (for segue: UIStoryboardSegue, sender: Any?){
+            if segue.identifier == "moveToDetail"{
+            if let photoDetailView = segue.destination as?
+            PhotoDetailViewController {
+                
+                if let photoToSend = sender as?
+                    Photos{
+            photoDetailView.photo = photoToSend
+                }
+            }
+        }
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
